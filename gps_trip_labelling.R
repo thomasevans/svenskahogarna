d2e4c54967f90f6c7d39ead1e8f744a2db107408
thomasevans <thomas.jude.evans@gmail.com>
2017-09-15 17:53:26 +0100

    Script with pseudo code for distance calculations on GPS data

---
+++ gps_trip_labelling.R
@@ -0,0 +1,26 @@
+# Load in GPS data together with data about nest location
+# Calculate distance to nest
+# Use to work out whether on foraging trip or not
+# Label foraging trips (make unique number IDs)
+# Output to table in DB, fields: device, date_time, distance, trip(bool), trip_id
+
+
+# Connect to DB and load in data -----
+
+# Calculate distance to nest -----
+# Function to calculate distance
+
+# Calculate distance to nest
+
+
+# Calculate p2p distances
+
+# Foraging trip labelling -----
+# Use to work out whether on foraging trip or not
+
+# Label foraging trips (make unique number IDs)
+
+
+# Output to DB ------
+
+# Output to table in DB, fields: device, date_time, distance, trip(bool), trip_id
