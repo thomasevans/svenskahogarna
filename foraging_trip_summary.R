33c5076ed2f0a0d08666476892b58c21fe111652
thomasevans <thomas.jude.evans@gmail.com>
2017-09-18 21:29:05 +0100

    Pseudocode for foraging trip summary info

---
+++ foraging_trip_summary.R
@@ -0,0 +1,24 @@
+# Summarise foraging trip info from GPS data 
+
+
+# Load in GPS data from database -------
+
+
+# Check datetime format etc (including timezone)
+# Fix if needed
+
+
+# Summarise trips ------
+# Use plyr::summarise
+
+
+
+# Output to database ------
+
+# Check format
+
+# Output
+
+
+
+
