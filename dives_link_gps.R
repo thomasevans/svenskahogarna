5eb745e5571ba2f5f92d580751a4829db101324f
thomasevans <thomas.jude.evans@gmail.com>
2017-09-19 10:24:40 +0100

    Link dive to GPS data

---
+++ dives_link_gps.R
@@ -0,0 +1,40 @@
+# Connect diving and GPs data
+# Get closest GPS location for each dive (where there is one)
+
+
+# Get data -------
+
+# Link to DB
+
+
+# Get dive data
+
+
+# Get GPS data
+
+
+# For each dive get GPS -----
+
+
+# Get all GPS locations from 600 s before and after dives
+
+
+# Calculate time before/ after for all GPS points
+
+
+# Get point with minimum time difference
+
+# Record
+# -date_time
+# -device_id
+# -dive_id
+# -lat
+# -long
+# -tdiff (before or after with sign)
+# -velocity (instantaneous from GPS)
+
+
+# Output to DB -------
+
+
+
